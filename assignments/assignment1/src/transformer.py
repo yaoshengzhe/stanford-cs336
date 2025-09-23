5171eb81cca63973828ab5a3c3ea36cd9d860276
Shengzhe <yaoshengzhe@gmail.com>
2025-09-22 21:55:18 -0700

    Implement RoPE

--- assignments/assignment1/src/transformer.py
+++ assignments/assignment1/src/transformer.py
@@ -21,6 +21,7 @@ class Linear(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return torch.einsum('... i, ji -> ... j', x, self.weights)
 
+
 class Embedding(nn.Module):
     def __init__(self,
                  vocab: int,
@@ -35,7 +36,8 @@ class Embedding(nn.Module):
         nn.init.trunc_normal_(self.weights, std=1, a=-3, b=3)
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
-        return self.weights[token_ids]
+        return self.weights[token_ids] # seq_len, d_model
+
 
 class RMSNorm(nn.Module):
     def __init__(self,
@@ -62,6 +64,7 @@ class RMSNorm(nn.Module):
 
         return result.to(in_dtype)
 
+
 class SwiGLU(nn.Module):
     def __init__(self,
                  d_model: int,
@@ -84,3 +87,75 @@ class SwiGLU(nn.Module):
         x = xw1 * torch.sigmoid(xw1) * xw3 # [..., d_ff]
 
         return torch.einsum('... j, ij -> ... i', x, self.w2)
+
+
+class RotaryPositionalEmbedding(nn.Module):
+    def __init__(self,
+                 theta: float,
+                 d_k: int,
+                 max_seq_len: int,
+                 device=None):
+        super().__init__()
+
+        self.theta = theta
+        self.d_k = d_k
+        self.max_seq_len = max_seq_len
+
+#        x1, x2, x3, x4, x5, x6
+
+#        x1 cos(t1, 1) + x2 sin(t1, 1)
+#        -x1 sin(t1, 1) + x2 cos(t1, 1)
+#        x3 cos(t2, 2) + x4 sin(t2, 2)
+#        -x3 sin(t2, 2) + x4 cos(t2, 2)
+#        x5 cos(t3, 3) + x6 sin(t3, 3)
+#        -x5 sin(t3, 3) + x6 cos(t3, 3)
+
+#        x1 cos(t1, 1)    +   -x2 sin(t1, 1)
+#        x2 cos(t1, 1)    +    x1 sin(t1, 1)
+#        x3 cos(t2, 2)    +   -x4 sin(t2, 2)
+#        x4 cos(t2, 2)    +    x3 sin(t2, 2)
+#        x5 cos(t3, 3)    +   -x6 sin(t3, 3)
+#        x6 cos(t3, 3)    +    x5 sin(t3, 3)
+
+        #x1, x2 -> -x2, x1
+        #         0   1
+        #        -1   0
+        # first cos tensor
+        # shape: d_k // 2
+        index_k = torch.arange(d_k // 2, dtype=torch.float32)
+        # shape: d_k
+        index_k = torch.repeat_interleave(index_k, repeats=2)
+
+        # seq_len, d_k
+        positions = torch.arange(max_seq_len, dtype=torch.float32).unsqueeze(1).repeat(1, d_k)
+
+        power = -2*index_k / d_k
+        angles = positions * (theta ** power)
+
+        cos = torch.cos(angles)
+        sin = torch.sin(angles)
+
+        # print(f'ddddd: cos = {cos.shape}, sin = {sin.shape}, max_seq_len = {max_seq_len}')
+        # rotations: d_k, d_k
+        self.register_buffer('sin', sin, persistent=False)
+        self.register_buffer('cos', cos, persistent=False)
+
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        # x: [..., seq_len, d_k]
+        # token_positions: [..., seq_len]
+
+        return x * self.cos[token_positions] + self._rearrange(x) * self.sin[token_positions]
+
+    def _rearrange(self, x: torch.Tensor):
+        # take a tensor [q1, q2, q3, ..., qn] and rearrange it so that
+        # every pair [q1, q2], [q3, q4], ... becomes [-q2, q1], [-q4, q3], ....
+
+        # shape: [..., n] -> [..., n//2, 2]
+        x_pairs = x.view(*x.shape[:-1], -1, 2)
+
+        # apply transformation: [-q2, q1]
+        transformed = torch.stack([-x_pairs[..., 1], x_pairs[..., 0]], dim=-1)
+
+        # flatten last two dims back
+        return transformed.view(*x.shape)
--- assignments/assignment1/tests/adapters.py
+++ assignments/assignment1/tests/adapters.py
@@ -217,7 +217,10 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+
+    layer = transformer.RotaryPositionalEmbedding(theta, d_k, max_seq_len)
+
+    return layer(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
