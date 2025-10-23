db08c74e5bed05e7b11c3f2f1cf544f99afc2b05
Shengzhe Yao <yaoshengzhe@gmail.com>
2025-10-23 16:24:43 -0700

    Implement SiLU

--- assignments/assignment1/tests/adapters.py
+++ assignments/assignment1/tests/adapters.py
@@ -450,7 +450,7 @@ def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
         Float[Tensor,"..."]: of with the same shape as `in_features` with the output of applying
         SiLU to each element.
     """
-    raise NotImplementedError
+    return in_features / (1 + (-in_features).exp())
 
 
 def run_get_batch(
